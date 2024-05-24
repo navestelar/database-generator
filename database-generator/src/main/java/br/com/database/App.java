55ae56ab6715a4175cdbfa270acec3cf7941f887
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-05-24 15:31:44 -0300

    first commit

---
+++ database-generator/src/main/java/br/com/database/App.java
@@ -0,0 +1,17 @@
+package br.com.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+
+
+
+    }
+
+}
